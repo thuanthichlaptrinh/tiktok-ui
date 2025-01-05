297a3cd33eb1c52388ca1171a7b3af66279a0d4b
You <thuanxinhtrai63@gmail.com>
2025-01-05 13:25:06 +0700

    Bài 67:  Xây Dựng UI Phần Header - Lần 44

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,14 @@
 import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import {
+    faCircleQuestion,
+    faCircleXmark,
+    faEarthAsia,
+    faEllipsisVertical,
+    faKeyboard,
+    faMagnifyingGlass,
+    faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
 
@@ -9,8 +17,15 @@ import styles from './Header.module.scss';
 import images from '~/assets/images';
 import AccountItem from '../AccountItem';
 import Button from '~/components/Button';
+import Menu from '~/components/Popper/Menu';
 
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    { icon: <FontAwesomeIcon icon={faEarthAsia} />, title: 'English' },
+    { icon: <FontAwesomeIcon icon={faCircleQuestion} />, title: 'Feedback and Help', to: '/feedback' },
+    { icon: <FontAwesomeIcon icon={faKeyboard} />, title: 'Keyboard shortcuts' },
+];
 
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
@@ -19,13 +34,13 @@ function Header() {
         setTimeout(() => {
             setSearchResult([]);
         }, 0);
-    });
+    }, []);
 
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <div className={cx('logo')}>
-                    <img src={images.logo.default} alt="Tiktok"></img>
+                    <img src={images.logo.default} alt="Tiktok" />
                 </div>
 
                 <Tippy
@@ -45,14 +60,10 @@ function Header() {
                 >
                     <div className={cx('search')}>
                         <input placeholder="Search accounts and videos" spellCheck={false} />
-                        {/* Close */}
                         <button className={cx('clear')}>
                             <FontAwesomeIcon icon={faCircleXmark} />
                         </button>
-                        {/* Loading */}
                         <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                        {/* Search */}
-
                         <button type="submit" className={cx('search-btn')}>
                             <FontAwesomeIcon icon={faMagnifyingGlass} />
                         </button>
@@ -62,6 +73,12 @@ function Header() {
                 <div className={cx('action')}>
                     <Button text>Upload</Button>
                     <Button primary>Log in</Button>
+
+                    <Menu items={MENU_ITEMS}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
                 </div>
             </div>
         </header>
